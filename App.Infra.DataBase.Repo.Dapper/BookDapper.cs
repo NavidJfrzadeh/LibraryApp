31e2b9929a84080f87462e9e402318bc698a5536
NavidJafarzadeh <navid.jfrzadeh@gmail.com>
2024-03-15 17:26:05 +0330

    add dapper

---
+++ App.Infra.DataBase.Repo.Dapper/BookDapper.cs
@@ -0,0 +1,31 @@
+﻿using App.Core;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.Design;
+
+namespace App.Infra.DataBase.Repo.Dapper
+{
+    public class BookDapper
+    {
+        public List<Book> GetBooks()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CW21;Integrated Security=True;TrustServerCertificate=True;";
+            using var cn = new SqlConnection(connectionString);
+            var sql = "Select * from dbo.books";
+            var cmd = new CommandDefinition(sql);
+            var result = cn.Query<Book>(cmd);
+            return result.ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CW21;Integrated Security=True;TrustServerCertificate=True;";
+            using var cn = new SqlConnection(connectionString);
+            var sql = "Select * from dbo.books as b" +
+                "Where b.Id = @Id ";
+            var cmd = new CommandDefinition(sql, new { Id = id });
+            var result = cn.QuerySingle<Book>(cmd);
+            return result;
+        }
+    }
+}
